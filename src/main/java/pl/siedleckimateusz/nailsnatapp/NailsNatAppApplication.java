170084d66a9f29a4d6f416717ee49de6c28b98db
SiedleckiMateusz <siedleckimateus@gmail.com>
2020-09-09 16:31:09 +0200

    Initial commit

---
+++ src/main/java/pl/siedleckimateusz/nailsnatapp/NailsNatAppApplication.java
@@ -0,0 +1,13 @@
+package pl.siedleckimateusz.nailsnatapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NailsNatAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NailsNatAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/siedleckimateusz/nailsnatapp/NailsNatAppApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.siedleckimateusz.nailsnatapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NailsNatAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
